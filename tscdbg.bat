afa3b57819d3b8456c534757377c15cf2d2dc308
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2021-01-17 09:34:21 -0300

    Update tscdbg.bat

--- tscdbg.bat
+++ tscdbg.bat
@@ -4,6 +4,6 @@ DEL assets\js\scripts.min.js
 
 REM Refer to tscmin.bat for an explanation of the final script file
 
-CALL tsc --target ES2017
+CALL tsc
 
 MOVE assets\js\scripts.js assets\js\scripts.min.js
