b6b6a70a038345c74ad1e1291834bc97cea783ca
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2021-05-11 08:43:14 -0300

    Upgrading the version

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -24,7 +24,7 @@
 // https://github.com/carlosrafaelgn/pixel
 //
 
-const version = "20210308",
+const version = "20210511",
 	isPWA = (window.location.href.indexOf("pwa") >= 0),
 	isIOSOrSafari = (function () {
 		// https://stackoverflow.com/q/9038625/3569421
